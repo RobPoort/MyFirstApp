1c809f08eafe06c9cc578ccfed121a51a321976e
Rob Poort <robpoort@gmail.com>
2012-11-21 17:15:32 +0100

    eerste bestanden

---
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,20 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class DisplayMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_message);
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+	}
+
+}
